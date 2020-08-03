5da064b798255dd214263b58be6f625606383683
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2020-08-03 14:48:01 -0400

    Don't build from source on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,12 @@
-stack setup
-stack install
+REM stack setup
+REM stack install
 
+REM if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN% || exit 1
+REM echo %LIBRARY_BIN%
+REM rem AppData\Roaming\local\bin
+REM copy %APPDATA%\local\bin\*.exe %LIBRARY_BIN% || exit 1
+
+REM Until stack 2.0 is available for Windows, we repackage
+REM the release binary
 if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN% || exit 1
-echo %LIBRARY_BIN%
-rem AppData\Roaming\local\bin
-copy %APPDATA%\local\bin\*.exe %LIBRARY_BIN% || exit 1
+move pandoc-plot.exe %LIBRARY_BIN% || exit 1
