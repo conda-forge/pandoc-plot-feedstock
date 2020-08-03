79dad9c9a91e842ab50644b2586289e4f5b71b59
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2020-08-03 15:17:55 -0400

    Build environment

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #set -euo pipefail
 #IFS=$'\n\t'
+
+export PATH="$PREFIX/bin:$BUILD_PREFIX/bin:$PATH"
+export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
+export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
+
 stack setup
 stack install
 
