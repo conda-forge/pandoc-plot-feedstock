b31e841b9a17b415a725ef3281644fd6d1e0fb7a
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2021-06-14 19:02:06 -0400

    Build from source

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,3 @@
+stack setup
 if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN% || exit 1
-move pandoc-plot.exe %LIBRARY_BIN% || exit 1
+stack install pandoc-plot --local-bin-path %LIBRARY_BIN% || exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 mkdir -p "$PREFIX/bin"
+stack setup
+stack install pandoc-plot --local-bin-path .
 chmod +x pandoc-plot
 /bin/mv pandoc-plot "$PREFIX/bin"
